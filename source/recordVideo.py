7f2e533a03a5fd9f0f65c5990360016fddc0ad03
jmperezr <juanmaperez7@gmail.com>
2016-07-11 20:25:48 +0200

    Deteccion de etiquetas mediante google vision api

--- source/recordVideo.py
+++ source/recordVideo.py
@@ -2,14 +2,22 @@
 Keys
 ----
 q - exit
-SPACE - screenshot
+SPACE - screenshot and label detection
 '''
 
 import numpy as np
 import cv2
 import time
+import argparse
+import base64
+import httplib2
 
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
+# The url template to retrieve the discovery document for trusted testers.
+DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+ 
 print __doc__
 
 number = 0
@@ -44,9 +52,36 @@ while(cap.isOpened()):
         	if ch == ord('q'):
             		break
 		if ch == ord(' '):
-            		fn = time.strftime("%d-%m-%Y_%H:%M:%S")+".png"
-            		cv2.imwrite(fn, frame)
-            		print fn, 'saved'
+            		photo_file = time.strftime("%d-%m-%Y_%H:%M:%S")+".png"
+            		cv2.imwrite(photo_file, frame)
+            		print photo_file, 'saved'
+			# [START authenticate]
+    			credentials = GoogleCredentials.get_application_default()
+    			service = discovery.build('vision', 'v1', credentials=credentials, discoveryServiceUrl=DISCOVERY_URL)
+    			# [END authenticate]
+
+    			# [START construct_request]
+    			with open(photo_file, 'rb') as image:
+        			image_content = base64.b64encode(image.read())
+        			service_request = service.images().annotate(body={
+            				'requests': [{
+                				'image': {
+                    					'content': image_content.decode('UTF-8')
+                				},
+                				'features': [{
+                    					'type': 'LABEL_DETECTION',
+                    					'maxResults': 10
+                				}]
+            				}]
+        			})
+        		# [END construct_request]
+        
+			# [START parse_response]
+        		response = service_request.execute()
+			size = len(response['responses'][0]['labelAnnotations'])
+			for i in range(size):
+        			label = response['responses'][0]['labelAnnotations'][i]['description']
+				print "Found label: %s" %label
     	else:
         	break
 
